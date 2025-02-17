4c0ef638bb70ccb110e25c24d770d87ceff83ed1
Ben Murphy-Baum <ben@wavemetrics.com>
2025-02-17 13:23:18 -0800

    Registering LSP for 'igor' language

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -29,7 +29,7 @@ export function activate(context: ExtensionContext) {
   // Options to control the language client
   const clientOptions: LanguageClientOptions = {
     // Register the server for all documents by default
-    documentSelector: [{ scheme: "file", language: "*" }],
+    documentSelector: [{ scheme: "file", language: "igorpro" }],
     synchronize: {
       // Notify the server about file changes to '.clientrc files contained in the workspace
       fileEvents: workspace.createFileSystemWatcher("**/.clientrc"),
